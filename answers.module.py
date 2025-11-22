a818a990f04d983c4f09c463be7035b370549aec
mjj21d <mjj21d@fsu.edu>
2025-11-21 19:02:53 -0500

    Update answers.module.py

--- answers.module.py
+++ answers.module.py
@@ -5,7 +5,7 @@ def myrec(x):
 # f(x) = 2x - f(x-1), if x>0
 # funtion raises exception if x<0
   if x < 0:
-    raise Exception("Input must be >= 0)
+    raise Exception("Input must be >= 0")
 
   if x == 0
     return 0
